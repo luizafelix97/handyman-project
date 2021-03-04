b987f37d2e3c93242b3bf97c4df0fe97f964744a
Ana Luiza Félix <luizafelix97@outlook.com>
2021-03-03 23:42:07 -0300

    Criação do projeto.

---
+++ src/main/java/com/br/src/HandymanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.br.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandymanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandymanProjectApplication.class, args);
+	}
+
+}
